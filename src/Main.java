8496e775d0b56d6131c14313b9e9b2de93bd150e
njshouse <njshouse@oakland.edu>
2016-01-21 16:42:41 -0500

    conversion added

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,17 @@ public class Main {
                 System.out.println("Enter binary number value: ");
         Scanner scanner = new Scanner(System.in);
         String buffer = scanner.nextLine();
-        System.out.println("You entered: " + buffer + " as your input.");
+        int result = Binary2Decimal(buffer);
+        System.out.println("Binary: " + buffer + " represents decimal value of: " + result);
 
+    }
+    public static int Binary2Decimal(String binary){
+        int result = 0;
+        for(int i = 0; i < binary.length(); i++) {
+            int digit = Character.getNumericValue(binary.charAt(i));
+            int position = binary.length() - i -1;
+            result += digit * Math.pow(2, position);
+        }
+        return result;
     }
 }
