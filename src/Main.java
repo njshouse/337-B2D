d87f58783b3abb8d32e057274a19d8f08214d701
njshouse <njshouse@oakland.edu>
2016-01-21 16:18:52 -0500

    Added code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Binary to Decimal Converter\n");
+                System.out.println("Enter binary number value: ");
+        Scanner scanner = new Scanner(System.in);
+        String buffer = scanner.nextLine();
+        System.out.println("You entered: " + buffer + " as your input.");
+
+    }
+}
